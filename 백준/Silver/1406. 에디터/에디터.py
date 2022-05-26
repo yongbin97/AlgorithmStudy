ec553e4d0e3c30b3fa5028274336c6aff72aadbb
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2022-05-26 16:11:14 +0900

    Update 에디터.py

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -23,3 +23,33 @@ for _ in range(int(sys.stdin.readline())):
 
 st1.extend(reversed(st2))
 print(''.join(st1))
+
+# import sys
+#
+# init_str = sys.stdin.readline().strip()
+# command_num = int(sys.stdin.readline())
+#
+# str_list = list(init_str)
+# curr_cursor = len(str_list)
+#
+# for i in range(command_num):
+#     command = list(sys.stdin.readline().split())
+#     if command[0] == "L":
+#         if curr_cursor > 0:
+#             curr_cursor -= 1
+#
+#     elif command[0] == "D":
+#         if curr_cursor < len(str_list):
+#             curr_cursor += 1
+#
+#     elif command[0] == "B":
+#         if curr_cursor != 0:
+#             del str_list[curr_cursor - 1]
+#             curr_cursor -= 1
+#     else:
+#         str_list.insert(curr_cursor, command[1])
+#         curr_cursor += 1
+#
+# print(''.join(str_list))
+# 위 방식에서 insert와 del은 O(n)의 시간복잡도를 가진다.
+# 최악의 경우 문자열 최대 길이 * 명령어 최대 개수 =>  100,000 * 500,000 만큼의 시간이 필요하다.
