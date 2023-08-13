55a847a69fac126b935e143ee37441b51237f37f
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-08-13 23:07:12 +0900

    ch.7 2_trapping_rain_water

--- 파이썬 알고리즘 인터뷰/7장/2_trapping_rain_water.py
+++ 파이썬 알고리즘 인터뷰/7장/2_trapping_rain_water.py
@@ -1,32 +1,30 @@
-from typing import List, Optional
+from typing import List
 
 
 class Solution:
-    @staticmethod
-    def strip_by_zero(height: List[int]) -> Optional[List[int]]:
-        left = 0
-        right = len(height) - 1
-        while height[left] == 0 or height[right] == 0:
-            if left == right:
-                return None
-            if height[left] == 0:
-                left += 1
-            if height[right] == 0:
-                right -= 1
-        return height[left: right + 1]
-
     def trap(self, height: List[int]) -> int:
-        max_h = max(height)
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
         water = 0
-        for _ in range(max_h):
-            height = self.strip_by_zero(height)
-            water += height.count(0)
-            height = [h-1 if h else h for h in height]
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+
+            if left_max <= right_max:
+                water += left_max - height[left]
+                left += 1
+            else:
+                water += right_max - height[right]
+                right -= 1
+
         return water
+
 
 
 height1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 height2 = [4, 2, 0, 3, 2, 5]
+height3 = [4, 2, 3]
 solution = Solution()
 print(solution.trap(height1))
 print(solution.trap(height2))
+print(solution.trap(height3))
