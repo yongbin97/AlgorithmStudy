50156fefad9c2e684dc4a13ba2220ee478acc1e4
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-08-18 23:44:29 +0900

    level0 문자열 반복해서 출력하기

---
+++ 프로그래머스_코틀린/level0_문자열_반복해서_출력하기.kt
@@ -0,0 +1,11 @@
+package 프로그래머스_코틀린
+
+fun main(args: Array<String>) {
+    val input = readln().split(" ")
+    val str = input[0]
+    val n: Int = input[1].toInt()
+
+    for (i in 1..n){
+        print(str)
+    }
+}
