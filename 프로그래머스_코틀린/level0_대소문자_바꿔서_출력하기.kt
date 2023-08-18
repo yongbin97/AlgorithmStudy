b1dc495e6579646f8edc6635a39144ee13d974dd
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-08-18 23:49:56 +0900

    level0 대소문자 바꿔서 출력하기

---
+++ 프로그래머스_코틀린/level0_대소문자_바꿔서_출력하기.kt
@@ -0,0 +1,12 @@
+package 프로그래머스_코틀린
+
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    for (s in s1){
+        if (s.isUpperCase()){
+            print(s.toLowerCase())
+        }else{
+            print(s.toUpperCase())
+        }
+    }
+}
