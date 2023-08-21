3a0070a7f393fb40bc693f1cb155ada4657410bc
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-08-21 22:43:19 +0900

    level2 해시 - 의상

---
+++ 프로그래머스_코틀린/level2/해시 의상.kt
@@ -0,0 +1,25 @@
+package 프로그래머스_코틀린.level2
+
+class Solution {
+    fun solution(clothes: Array<Array<String>>): Int {
+        var clothMap = HashMap<String, Int>()
+        for (cloth in clothes){
+            if (clothMap.containsKey(cloth[1])){
+                clothMap[cloth[1]] = clothMap[cloth[1]]!! + 1
+            } else{
+                clothMap[cloth[1]] = 1
+            }
+        }
+        var result = 1
+        for ((key, item) in clothMap){
+            result *= (item + 1)
+        }
+        return result - 1
+    }
+
+    fun solution2(clothes: Array<Array<String>>) = clothes
+        .groupBy { it[1] }.values   // group by type of clothes, keep only names of clothes
+        .map { it.size + 1 }        // number of things to wear in a group (including wearing nothing)
+        .reduce(Int::times)         // combine
+        .minus(1)
+}
