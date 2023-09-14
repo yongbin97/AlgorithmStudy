94ed3603478604c94124c5feb745a1173c728e17
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-09-14 09:46:59 +0900

    리트코드 20번

--- 파이썬 알고리즘 인터뷰/8장/20 valid parentneses.py
+++ 파이썬 알고리즘 인터뷰/8장/20 valid parentneses.py
@@ -16,3 +16,21 @@ class Solution:
                     return False
 
         return len(stack) == 0
+
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        parentheses_table = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+
+        stack = []
+        for c in s:
+            if parentheses_table.get(c) is None:
+                stack.append(c)
+
+            elif not stack or parentheses_table.get(c) != stack.pop():
+                return False
+
+        return len(stack) == 0
