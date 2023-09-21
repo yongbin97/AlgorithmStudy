37bdeae54bfc12b8b699131372683492d65e4760
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2023-09-21 15:48:55 +0900

    [Silver III] Title: 프린터 큐, Time: 80 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,33 +1,25 @@
 import sys
 from collections import deque
 
-testcase_num = int(sys.stdin.readline())
 
-for _ in range(testcase_num):
-    n, m = map(int, sys.stdin.readline().split())
-    prior_list = deque(map(int, sys.stdin.readline().split()))
+test_count = int(sys.stdin.readline())
 
-    idx_list = deque(range(1, n+1))
-    target = idx_list[m]
-    count = 1
-    maxPrior = max(prior_list)
+for _ in range(test_count):
+    N, M = map(int, sys.stdin.readline().split())
+    x = sys.stdin.readline().split()
+    tuple_list = deque((int(p), i) for i, p in enumerate(x))
 
-    while len(idx_list) != 0:
-
-        idx = idx_list.popleft()
-        prior = prior_list.popleft()
-
-        if prior == maxPrior:
-            if idx == target:
-                print(count)
-                break
-            else:
-                maxPrior = max(prior_list)
-                count += 1
+    answer = 1
+    while tuple_list:
+        curr_p, curr_i = tuple_list.popleft()
+        if any(curr_p  < p for p, _ in tuple_list):
+            tuple_list.append((curr_p, curr_i))
 
         else:
-            idx_list.append(idx)
-            prior_list.append(prior)
+            if curr_i == M:
+                print(answer)
+            else:
+                answer += 1
 
 
 
