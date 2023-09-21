cb6302d80564d67b22a3880c001b1664c3e32f5a
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2023-09-21 10:57:55 +0900

    파이썬 알고리즘 인터뷰 23번

---
+++ 파이썬 알고리즘 인터뷰/9장/23 implement stack using queue.py
@@ -0,0 +1,24 @@
+class MyStack:
+
+    def __init__(self):
+        self._stack = []
+
+    def push(self, x: int) -> None:
+        self._stack.append(x)
+
+    def pop(self) -> int:
+        return self._stack.pop()
+
+    def top(self) -> int:
+        return self._stack[-1]
+
+    def empty(self) -> bool:
+        return len(self._stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
