2dc6b1f486dbc33246ab7fadd7b55467697b50b7
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2023-09-30 17:32:58 +0900

    파이썬 알고리즘 인터뷰 40번

---
+++ 파이썬 알고리즘 인터뷰/40 Network Delay Time.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import deque, defaultdict
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # visited[a] => time of n node receive signal
+        visited_time = [-1] * n
+        network = defaultdict(list)
+        for start, end, time in times:
+            network[start - 1].append((end - 1, time))
+        dq = deque()
+
+        visited_time[k - 1] = 0
+        dq.append(k - 1)
+        while dq:
+            curr_node = dq.popleft()
+            for end, time in network[curr_node]:
+                # not visited or update min visited value
+                if visited_time[end] == -1 or visited_time[curr_node] + time < visited_time[end]:
+                    visited_time[end] = visited_time[curr_node] + time
+                    dq.append(end)
+
+        if -1 in visited_time:
+            return -1
+        else:
+            return max(visited_time)
+
+
+solution = Solution()
+print(solution.networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2))
+print(solution.networkDelayTime([[1, 2, 1]], 2, 1))
+print(solution.networkDelayTime([[1, 2, 1], [2, 3, 2], [1, 3, 4]], 3, 1))
