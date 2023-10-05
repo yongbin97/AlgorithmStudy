0dcb5b6244151ca54238d6d3186df0a47a810db1
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-10-05 21:03:48 +0900

    파이썬 알고리즘 인터뷰 78번

---
+++ 파이썬 알고리즘 인터뷰/78 best time to buy and sell stock2.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(len(prices) - 1):
+            if prices[i + 1] > prices[i]:
+                result += prices[i + 1] - prices[i]
+        return result
+
+
+solution = Solution()
+print(solution.maxProfit(prices=[7, 1, 5, 3, 6, 4]))
