df507c4ceffdd0004dfb8a4676cc7cf0779ea840
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2023-10-17 13:08:49 +0900

    [level unrated] Title: 평행, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+// 12 34 / 13 24/ 14 23
+
+double getGradient(vector<int> dot1, vector<int> dot2){
+    return fabs(dot1[1] - dot2[1])/fabs(dot1[0] - dot2[0]);
+}
+
+int solution(vector<vector<int>> dots) {
+    int answer = 0;
+    
+    // {1, 2} {3, 4}
+    if (getGradient(dots[0], dots[1]) == getGradient(dots[2], dots[3])){
+        return 1;
+    }
+    else if (getGradient(dots[0], dots[2]) == getGradient(dots[1], dots[3])){
+        return 1;
+    }
+    else if (getGradient(dots[0], dots[3]) == getGradient(dots[1], dots[2])){
+        return 1;
+    }
+    
+    
+    return answer;
+}
