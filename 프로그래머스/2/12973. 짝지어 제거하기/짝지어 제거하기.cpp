8d0577c555104eac4275bf867f2abbcc324b24e3
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2023-10-19 22:42:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.42 ms, Memory: 6.54 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++){
+        if (st.size() > 0 && st.top() == s[i]){
+            st.pop();
+        }
+        else{
+            st.push(s[i]);
+        }
+    }
+    
+    if (st.size() == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
