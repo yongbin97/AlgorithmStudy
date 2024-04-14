361f4b69dd8a7002761a62ea3c76f8f1b918a0da
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-04-14 13:52:49 +0900

    LeetCode -561.Array Partition

---
+++ 리트코드_자바/561.Array Partition/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0){
+                answer += nums[i];
+            }
+        }
+        return answer;
+    }
+}
