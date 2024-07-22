2622c94fc9efe9ea5ed88e7454db59026ca5f97b
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-07-22 22:45:49 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 17.65 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,48 +1,33 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0; 
+        int answer = 0;
         
-        Map<String, Map<String, Integer>> fromToCountMap = new HashMap<>();
-        Map<String, Integer> giftIndexMap = new HashMap<>();
-        
-        // init Map
-        for (String friend: friends) {
-            fromToCountMap.put(friend, new HashMap<>());
-            giftIndexMap.put(friend, 0);
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.length; i++){
+            map.put(friends[i], i);
         }
         
-        // set map
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+        
         for (String gift: gifts) {
-            String[] fromTo = gift.split(" ");
+            String[] curr = gift.split(" ");
             
-            fromToCountMap.get(fromTo[0]).putIfAbsent(fromTo[1], 0);
-            fromToCountMap.get(fromTo[1]).putIfAbsent(fromTo[0], 0);
-            fromToCountMap.get(fromTo[0]).put(fromTo[1], fromToCountMap.get(fromTo[0]).get(fromTo[1]) + 1);
-            fromToCountMap.get(fromTo[1]).put(fromTo[0], fromToCountMap.get(fromTo[1]).get(fromTo[0]) - 1);
-            
-            giftIndexMap.put(fromTo[0], giftIndexMap.get(fromTo[0]) + 1);
-            giftIndexMap.put(fromTo[1], giftIndexMap.get(fromTo[1]) - 1);
+            index[map.get(curr[0])] ++;
+            index[map.get(curr[1])] --;
+            record[map.get(curr[0])][map.get(curr[1])] ++;
         }
         
-        
-        // get answer
-        for (String fromFriend: friends) {
-            int count = 0;
-            Map<String, Integer> fromCountMap = fromToCountMap.get(fromFriend);
-            for (String toFriend: friends) {
-                if (fromFriend.equals(toFriend)) continue;
-                
-                if (!fromCountMap.containsKey(toFriend) || fromCountMap.get(toFriend) == 0) {
-                    if (giftIndexMap.get(fromFriend) > giftIndexMap.get(toFriend)) count ++;
-                } else {
-                    if (fromCountMap.get(toFriend) > 0) count ++;
-                }
+        for (int i = 0; i < friends.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < friends.length; j++) {
+                if (i == j) continue;
+                if (record[i][j] > record[j][i]) cnt ++;
+                else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt ++;
             }
-            System.out.println(fromFriend + " : " + count);
-            answer = Math.max(answer, count);
+            answer = Math.max(answer, cnt);
         }
         
         return answer;
