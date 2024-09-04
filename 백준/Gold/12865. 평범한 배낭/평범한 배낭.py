c3ee396692327e1076c37e94d7ba9ef23719fc8a
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-09-04 14:54:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 110024 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,26 @@
+"""
+물건 N개
+무게 W, 가치 V
+K만큼 담을 수 있는 배낭
+"""
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+answer = 0;
+dp = [0] * (K + 1)
+wv_list = []
+
+for _ in range(N):
+    W, V = map(int, sys.stdin.readline().split())
+    wv_list.append([W, V])
+
+wv_list.sort(key=lambda x: (-x[1], x[0]))
+
+for w, v in wv_list:
+    for idx in range(K, w - 1, -1):
+        dp[idx] = max(dp[idx], dp[idx - w] + v)
+        answer = max(answer, dp[idx])
+
+print(answer)
+
