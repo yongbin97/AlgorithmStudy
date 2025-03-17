2b78de460e3f894347f9aba0c6d13d8f0b09586d
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2025-03-17 13:48:39 +0900

    [level 2] Title: 전화번호 목록, Time: 357.65 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        
-        // Arrays.sort(phone_book, (o1, o2) -> {
-        //     return o1.length() - o2.length();
-        // });
         Arrays.sort(phone_book);
         
-        System.out.println(Arrays.toString(phone_book));
-        
-        
-        return answer;
+        for (int i = 0; i < phone_book.length - 1; i ++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
